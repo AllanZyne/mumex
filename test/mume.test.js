0f56380173f3fd5fadaffedf9762eb99a16de9f8
Allan Zyne <allanzyne@outlook.com>
2020-03-22 15:24:19 +0800

    WIP: ...

---
+++ test/mume.test.js
@@ -0,0 +1,42 @@
+const mume = require("../out/src/mume");
+
+async function main() {
+  await mume.init();
+
+  const engine = new mume.MarkdownEngine({
+    filePath: "./test.md",
+    config: {
+      previewTheme: "github-light.css",
+      // revealjsTheme: "white.css"
+      codeBlockTheme: "default.css",
+      printBackground: true,
+      enableScriptExecution: true, // <= for running code chunks
+      mathRenderingOption: "MathJax",
+    },
+  });
+
+  // open in browser
+  // await engine.openInBrowser({ runAllCodeChunks: true });
+
+  // // html export
+  await engine.htmlExport({ offline: false, runAllCodeChunks: true });
+
+  // // chrome (puppeteer) export
+  // await engine.chromeExport({ fileType: "pdf", runAllCodeChunks: true }); // fileType = 'pdf'|'png'|'jpeg'
+
+  // // prince export
+  // await engine.princeExport({ runAllCodeChunks: true });
+
+  // // ebook export
+  // await engine.eBookExport({ fileType: "epub" }); // fileType = 'epub'|'pdf'|'mobi'|'html'
+
+  // // pandoc export
+  // await engine.pandocExport({ runAllCodeChunks: true });
+
+  // // markdown(gfm) export
+  // await engine.markdownExport({ runAllCodeChunks: true });
+
+  return process.exit();
+}
+
+main();
---
+++ test/test.md
@@ -0,0 +1,11 @@
+# Markdown For Typora
+
+## Overview
+
+inline: $f(x) = x^2$
+block: $$\frac{1}{\sqrt{2\pi}\sigma}e^{\frac{(\mu-1)^2}{2\sigma^2}}$$
+
+:smile:
+:far-square: :fas-square:
+:fa-car:
+:fas-car:
